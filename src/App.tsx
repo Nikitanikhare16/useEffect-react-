8a3c2258f2151a288e67d42f9e62597fe37110bb
Nikitanikhare16 <109062396+Nikitanikhare16@users.noreply.github.com>
2024-05-08 16:35:45 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+
 import './App.css';
+import { resolve } from 'path';
 
 function App() {
+
+  const[title,setTitle] = useState('no change');
+  const userApi = ()=>{
+return new Promise <any>((resolve,reject)=>{
+  return setTimeout(()=>{
+    resolve({name:'nikita'})
+  },1000)
+})
+  }
+  const getUserData=async() =>{
+const userData = await userApi();
+console.log('userData',userData);
+const {name} = userData;
+setTitle(name);
+  }
+  useEffect(()=>{
+getUserData();
+
+  },[]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    use Effect{title}
+  
+  </div>
   );
 }
 
